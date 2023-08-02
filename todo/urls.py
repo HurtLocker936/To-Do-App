81278842657c0a5f8ffa02dce6c5540183d2ef7f
HurtLocker936 <shashankism936@gmail.com>
2023-08-02 11:13:59 +0530

    new commit

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('addtask/', views.AddTask, name='addtask'),
+    path('mark_as_done/<int:pk>/', views.MarkTask, name='mark_as_done'),
+    path('delete_task/<int:pk>', views.DeleteTask, name='delete_task'),
+    path('mark_as_undone/<int:pk>', views.MarkUndone, name='mark_as_undone'),
+    path('edit_task/<int:pk>', views.EditTask, name='edit_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -9,3 +9,30 @@ def AddTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+
+def MarkTask(request, pk):
+    task_obj = Task.objects.get(id=pk)
+    task_obj.is_completed = True
+    task_obj.save()
+    return redirect('home')
+
+
+def DeleteTask(request, pk):
+    task_obj = Task.objects.get(id=pk)
+    task_obj.delete()
+    return redirect('home')
+
+
+def MarkUndone(request, pk):
+    task_obj = Task.objects.get(id=pk)
+    task_obj.is_completed = False
+    task_obj.save()
+    return redirect('home')
+
+def EditTask(request, pk):
+    task = request.POST['task']
+    task_obj = Task.objects.get(id=pk)
+    task_obj.task = task
+    task_obj.save()
+    return redirect('home')
